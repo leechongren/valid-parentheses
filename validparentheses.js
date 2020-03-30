a7b1b4495439f4acea30b5eba3a64e9f0c24f9c5
leechongren <lee_chongren@hotmail.com>
2020-03-31 00:51:33 +0800

    add additional tests for front and back of input

--- validparentheses.js
+++ validparentheses.js
@@ -1,7 +1,14 @@
 const validparentheses = input => {
+  const splitInput = input.split("");
+
   if (input === ")" || input === "(") {
     return "Invalid";
+  } else if (splitInput[0] === ")") {
+    return "Invalid";
+  } else if (splitInput[splitInput.length - 1] === "(") {
+    return "Invalid";
   }
+
   return "Valid";
 };
 
