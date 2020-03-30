3518d721b9ea89241bb6aaecb5521d82f619a5d4
leechongren <lee_chongren@hotmail.com>
2020-03-31 00:53:01 +0800

    remove excess code for base case

--- validparentheses.js
+++ validparentheses.js
@@ -1,9 +1,7 @@
 const validparentheses = input => {
   const splitInput = input.split("");
 
-  if (input === ")" || input === "(") {
-    return "Invalid";
-  } else if (splitInput[0] === ")") {
+  if (splitInput[0] === ")") {
     return "Invalid";
   } else if (splitInput[splitInput.length - 1] === "(") {
     return "Invalid";
